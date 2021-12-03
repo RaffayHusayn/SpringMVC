f18314c5b9ed1c50fcf560478df727fc1cd067a5
Raffay Syed <raffayhusayn@gmail.com>
2021-12-03 00:22:28 -0500

    Replace HttpServletRequest and HttpServletResponse object from AddController with annotation
    -- replace HttpServletRequest with @RequestParam(num1) // num1 is the form input variable name
    -- replace HttpsServletResponse because we are not using response object

--- src/main/java/com/fclass/AddController.java
+++ src/main/java/com/fclass/AddController.java
@@ -2,7 +2,10 @@ package com.fclass;
 
 import com.fclass.service.AddService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,9 +16,10 @@ import javax.servlet.http.HttpServletResponse;
 public class AddController {
 
     @RequestMapping("/add") //name of action in the form in index.jsp, doesn't depend on the name of class or method or anything other than mapping
-   public ModelAndView show(HttpServletRequest request, HttpServletResponse response){
-        int num1 = Integer.parseInt(request.getParameter("num1")); //works with both get and post method
-        int num2 = Integer.parseInt(request.getParameter("num2")); //works with both get and post method
+   public ModelAndView show(@RequestParam("num1") int  num1, @RequestParam("num2") int num2){
+//        int num1 = Integer.parseInt(request.getParameter("num1")); //works with both get and post method
+//        int num2 = Integer.parseInt(request.getParameter("num2")); //works with both get and post method
+
         AddService addService = new AddService();
         int add = addService.add(num1, num2);// in complicated applications, controllers don't do processing so we are just
         //following that
